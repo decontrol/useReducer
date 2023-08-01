b6358141cbedf4a1810af6e3cdd53ea36660b30c
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-08-01 13:13:30 -0400

    completed

---
+++ src/Todo.js
@@ -0,0 +1,30 @@
+import { ACTIONS } from './App'
+export const Todo = ({ todo, dispatch }) => {
+	return (
+		<div>
+			<span style={{ color: todo.complete ? '#aaa' : '#000' }}>
+				{todo.name}
+			</span>
+			<button
+				onClick={() =>
+					dispatch({
+						type: ACTIONS.TOGGLE_TODO,
+						payload: { id: todo.id },
+					})
+				}
+			>
+				Toggle
+			</button>
+			<button
+				onClick={() =>
+					dispatch({
+						type: ACTIONS.DELETE_TODO,
+						payload: { id: todo.id },
+					})
+				}
+			>
+				Delete
+			</button>
+		</div>
+	)
+}
